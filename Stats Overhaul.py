c989df5c01d64851d11e8ecdd7280a4d352d110b
Xnightslayer99 <68559143+Xnightslayer99@users.noreply.github.com>
2021-12-25 21:26:07 -0600

    Create Stats Overhaul.py

---
+++ Stats Overhaul.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import random
+import string
+pname = input("What do you wish to be called?(This cannot be changed later)")
+class player:
+  def __init__(self, name):
+    self.name = pname
+    self.str
+    self.cc
+    self.cd
+    self.hp
+    self.maxhp
+    self.def
+    self.int
+    self.abildam
+    self.bdam = 1
+    self.ewdam = 0
+    self.armor
+    self.equipedArmor
+    self.equipedWeapon
